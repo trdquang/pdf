cf74d9471d603bf7761102d5196c33534121463c
kakaka <songtu362k1@gmail.com>
2025-01-03 21:38:50 +0700

    .

---
+++ js/index.js
@@ -0,0 +1,17 @@
+function featurePdf(id){
+    if(id == 1){
+        window.location.href="merge.html";
+    }
+    //---
+    else if(id == 2){
+        window.location.href="split.html";
+    }
+    //---
+    else if(id == 3){
+        window.location.href="to_word.html";
+    }
+    //---
+    else if(id == 4){
+        window.location.href="page_number.html";
+    }
+}
