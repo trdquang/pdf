e8a9c41197ffc6ef7412ea8e1d0f36e8ecc09718
kakaka <songtu362k1@gmail.com>
2025-01-07 22:32:13 +0700

    .

--- js/page_number.js
+++ js/page_number.js
@@ -5,22 +5,23 @@ async function handleFileSelect(event) {
     const file = event.target.files[0];
     if (file) {
         document.getElementById('addPageNumbers').disabled = false;
-        const arrayBuffer = await file.arrayBuffer();
-        const pdfDoc = await PDFLib.PDFDocument.load(arrayBuffer);
-        const pdfBytes = await pdfDoc.save();
-        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
-        const url = URL.createObjectURL(blob);
-        document.getElementById('pdfContainer').style.display = 'block';
-        const canvas = document.getElementById('pdfCanvas');
-        const context = canvas.getContext('2d');
-        const pdf = await pdfjsLib.getDocument({ url }).promise;
-        const page = await pdf.getPage(1);
-        const viewport = page.getViewport({ scale: 1.0 });
-        canvas.height = viewport.height;
-        canvas.width = viewport.width;
-        await page.render({ canvasContext: context, viewport }).promise;
+
+        const previewContainer = document.getElementById('previewContainer');
+        previewContainer.innerHTML = '';
+
+        const previewDiv = document.createElement('div');
+        previewDiv.classList.add('preview');
+
+        // Create the title div showing the file name
+        const titleDiv = document.createElement('div');
+        titleDiv.classList.add('file-title');
+        titleDiv.innerHTML = file.name;  // Use the file name
+
+        previewDiv.appendChild(titleDiv);  // Append the title to the preview div
+        previewContainer.appendChild(previewDiv);
     }
 }
+
 
 async function addPageNumbers() {
     const fileInput = document.getElementById('fileInput');
@@ -38,6 +39,7 @@ async function addPageNumbers() {
             y: 20,
             size: fontSize,
             font,
+            // color: PDFLib.rgb(0.941, 0.074, 0.235)
             color: PDFLib.rgb(0, 0, 0)
         });
     });
